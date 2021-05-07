d7b242c55f4ce5f547c6df89e6aae0c87ed58b4c
CNN-unitn <chiara.nardin@unitn.it>
2021-05-07 11:54:27 +0200

    FIXS -- exams/2018-06-14/testo-esame/K-sol.py
    ex3 on dictionaries

--- exams/2018-06-14/testo-esame/K-sol.py
+++ exams/2018-06-14/testo-esame/K-sol.py
@@ -27,54 +27,104 @@
 # assert doppie([5,3,8]) == [(5,10), (3,6), (8,16)]
 # # FINE TEST
 
+# # -----------------------------------------------------------------------------------------------------
+# # -----------------------------------------------------------------------------------------------------
+# # Esercizio 2
+# """
+# Quando si analizza una frase, può essere utile processarla per rimuovere parole molto comuni, come
+# gli articoli e le preposizioni:
+# Per esempio:
+# "un libro su Python per principianti"
+# si può semplificare in
+# "libro Python principianti"
+# Le parole 'poco utili' vengono chiamate 'stopwords'. Questo processo è per esempio eseguito
+# dai motori di ricerca per ridurre la complessità della stringa di input fornita dall'utente.
+# Implementare una funzione che prende una stringa e RITORNA la stringa di input senza le
+# stopwords
+# SUGGERIMENTO 1: le stringhe in Python sono *immutabili* ! Per rimuovere le parole dovete
+# creare una _nuova_ stringa a partire dalla stringa di partenza.
+# SUGGERIMENTO 2: create una lista di parole così:
+# lista = stringa.split(" ")
+# SUGGERIMENTO 3: operate le opportune trasformazioni su lista, e poi costruite la stringa
+# da restituire con " ".join(lista)
+# """
+# def nostop(stringa, stopwords):
+# 	readstr = stringa.split(" ")
+# 	print(readstr)
+
+# 	for i in range(0,len(stopwords)):
+# 		n = readstr.count(stopwords[i])
+# 		word2remove=stopwords[i]
+# 		print("Parola da rimuovere: \"",word2remove, "\" ",n," volte")
+
+# 		for j in range(0,n):
+# 			readstr.remove(word2remove)
+# 			print(readstr)
+# 			j =+ 1
+
+# 	print("finale", " ".join(readstr))
+# 	print("-" * 25)
+# 	return " ".join(readstr)
+
+
+# # INIZIO TEST - NON TOCCARE !
+# assert nostop("un", ["un"]) == ""
+# assert nostop("un", []) == "un"
+# assert nostop("", []) == ""
+# assert nostop("", ["un"]) == ""
+# assert nostop("un libro", ["un"]) == "libro"
+# assert nostop("un libro su Python", ["un","su"]) == "libro Python"
+# assert nostop("un libro su Python per principianti", ["un","uno","il","su","per"]) == "libro Python principianti"
+# # FINE TEST
+# # -----------------------------------------------------------------------------------------------------
+# # -----------------------------------------------------------------------------------------------------
+
 # -----------------------------------------------------------------------------------------------------
 # -----------------------------------------------------------------------------------------------------
-# Esercizio 2
+# Esercizio 3
 """
-Quando si analizza una frase, può essere utile processarla per rimuovere parole molto comuni, come
-gli articoli e le preposizioni:
-Per esempio:
-"un libro su Python per principianti"
-si può semplificare in
-"libro Python principianti"
-Le parole 'poco utili' vengono chiamate 'stopwords'. Questo processo è per esempio eseguito
-dai motori di ricerca per ridurre la complessità della stringa di input fornita dall'utente.
-Implementare una funzione che prende una stringa e RITORNA la stringa di input senza le
-stopwords
-SUGGERIMENTO 1: le stringhe in Python sono *immutabili* ! Per rimuovere le parole dovete
-creare una _nuova_ stringa a partire dalla stringa di partenza.
-SUGGERIMENTO 2: create una lista di parole così:
-lista = stringa.split(" ")
-SUGGERIMENTO 3: operate le opportune trasformazioni su lista, e poi costruite la stringa
-da restituire con " ".join(lista)
+Restituisce un dizionario con n coppie chiave-valore, dove le chiavi sono
+numeri interi da 1 a n incluso, e ad ogni chiave i è associata una lista di numeri
+da 1 a i
+NOTA: le chiavi sono *numeri interi*, NON stringhe !!!!!
+Esempio:
+dilist(3)
+deve dare:
+{
+1:[1],
+2:[1,2],
+3:[1,2,3]
+}
 """
-def nostop(stringa, stopwords):
-	readstr = stringa.split(" ")
-	print(readstr)
-
-	for i in range(0,len(stopwords)):
-		n = readstr.count(stopwords[i])
-		word2remove=stopwords[i]
-		print("Parola da rimuovere: \"",word2remove, "\" ",n," volte")
-
-		for j in range(0,n):
-			readstr.remove(word2remove)
-			print(readstr)
-			j =+ 1
-
-	print("finale", " ".join(readstr))
-	print("-" * 25)
-	return " ".join(readstr)
 
 
-# INIZIO TEST - NON TOCCARE !
-assert nostop("un", ["un"]) == ""
-assert nostop("un", []) == "un"
-assert nostop("", []) == ""
-assert nostop("", ["un"]) == ""
-assert nostop("un libro", ["un"]) == "libro"
-assert nostop("un libro su Python", ["un","su"]) == "libro Python"
-assert nostop("un libro su Python per principianti", ["un","uno","il","su","per"]) == "libro Python principianti"
+def dilist(n):
+# # scrivi qui
+	diz = dict()
+	for voce in range(1,n+1):
+		diz[voce] = list(range(1,voce+1))
+#		print("incremento diz;" , diz)
+		
+	print("Diz.: ", diz)
+	print("-"*25)
+	return diz 
+
+#INIZIO TEST - NON TOCCARE !
+assert dilist(0) == dict()
+assert dilist(1) == {
+1:[1]
+}
+assert dilist(2) == {
+1:[1],
+2:[1,2]
+}
+assert dilist(3) == {
+1:[1],
+2:[1,2],
+3:[1,2,3]
+}
 # FINE TEST
+
 # -----------------------------------------------------------------------------------------------------
 # -----------------------------------------------------------------------------------------------------
+# Esercizio 4
